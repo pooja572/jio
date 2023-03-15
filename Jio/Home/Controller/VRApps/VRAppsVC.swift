dcff25c1fd9d8c441940f30cf9652f93541e7ab4
Pooja <pooja@Pooja-MacBook-Pro.local>
2023-03-15 18:45:38 +0530

    first commit

--- Jio/Home/Controller/VRApps/VRAppsVC.swift
+++ Jio/Home/Controller/VRApps/VRAppsVC.swift
@@ -13,21 +13,6 @@ class VRAppsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-        let allApps = UIApplication.shared
-             for app in allApps.windows {
-                 for view in app.subviews {
-                     if let iconView = NSClassFromString("Docky.AppIconView") {
-                         if view.isKind(of: iconView) {
-                             if let icon = view.subviews.first?.subviews.first as? UIImageView, let name = icon.accessibilityLabel {
-                                 print("App Name: \(name)")
-                                 print("Icon: \(icon.image)")
-                             }
-                         }
-                     }
-                 }
-             }
-      
         collectionView.register(UINib(nibName: "LiveUserCell", bundle: nil), forCellWithReuseIdentifier: "LiveUserCell")
     }
     
